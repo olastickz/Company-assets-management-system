d91bf6104d2e649e94a065ebe7ed3657358e1d0e
Olamilekan <topafgg@gmail.com>
2026-07-15 08:19:15 -0400

    Prepare project for Render deployment

--- fleet_management/asset_management/settings.py
+++ fleet_management/asset_management/settings.py
@@ -148,8 +148,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'topafgg@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'mbqn xqaw kozx roaj')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Expiry alert email configuration
