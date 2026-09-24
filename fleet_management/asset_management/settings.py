8f629c4a12675c445c464cd35d15d4417ab4c907
Olamilekan <topafgg@gmail.com>
2026-09-24 07:10:40 -0400

    hardcore

--- fleet_management/asset_management/settings.py
+++ fleet_management/asset_management/settings.py
@@ -195,8 +195,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('topafgg@gmaail.com')
-EMAIL_HOST_PASSWORD = os.getenv('mtut nicv qyxa rjyf')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Expiry alert email configuration
